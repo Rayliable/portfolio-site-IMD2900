2011ede13c467275db98066c3ce239c39ce4e856
LiterallyDan <90472271+LiterallyDan@users.noreply.github.com>
2024-02-28 15:47:26 -0500

    Tags section added to upload image
    On top of the title and image itself you can now add a predefined tag. The default option is untagged which will allow for user to ignore that section if they like

--- home/models.py
+++ home/models.py
@@ -5,8 +5,18 @@ from django.db import models
 
 # user uploads mode (includes title, image, tags (not implemented yet))
 class UserUpload(models.Model):
+    untagged = 'option_one'
+
+    OPTIONS = [
+        (untagged, 'untagged'),
+        ('option_two', 'Photography'),
+        ('option_three', 'Illustration'),
+        ('option_four', 'Graphic Design'),
+    ]
+
     title = models.CharField(max_length=255)
     image = models.ImageField(upload_to='user_upload/')
+    tags = models.CharField(max_length=30, choices=OPTIONS, default=untagged)
 
     def __str__(self):
         return self.title
