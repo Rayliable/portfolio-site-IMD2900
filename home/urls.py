55fc35cff80a960aaa100f2661e6df4c40c45246
Daniel Gagne <DANIELGAGNE3@cunet.carleton.ca>
2024-03-05 09:44:09 -0500

    Add HTML files for pages
    Pages for HTMLs and added URL links so they work, but absolutely no actual information is on the pages.

--- home/urls.py
+++ home/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path("", views.home, name="home"),
     path("image_upload/", views.image_upload, name="image_upload"),
     path("upload_view/", views.upload_view, name="upload_view"),
+    path("about_us/", views.about, name="about_us"),
+    path("contact_us", views.contact, name="contact_us"),
 ]
