3b229dec05458a9ec129c9af046306ecf133b95b
Ray <rayrmrose@gmail.com>
2024-03-21 21:29:00 -0400

    Just clearin it all out to try again

--- register/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include
-from . import views
-
-urlpatterns = [
-    path('register/', views.CreateUserView.as_view(), name="register"),
-    path('<int:pk>/profile/', views.ProfileView.as_view(), name="profile"),
-    path('<int:pk>/edit_profile/', views.ProfileEditView.as_view(), name="edit_profile"),
-    path('logout/', views.logout_view, name="logout"),
-]
